79d9898b64d269c03b834521fd226dfd349e338a
Sirko Sittig <sirko.sittig@gmail.com>
2015-01-07 18:00:26 -0300

    adjust rise exceeded check method

--- stock.rb
+++ stock.rb
@@ -20,9 +20,9 @@ class Stock
     price_at_day1 = price_at(date: range.begin)
     price_at_day2 = price_at(date: range.end)
 
-    price_rise          = diff(price_at_day2, price_at_day1)
-    one_percent_of_day1 = calc_percents(number: price_at_day1)
+    price_rise      = diff(price_at_day2, price_at_day1)
+    percent_of_day1 = calc_percents(number: price_at_day1, percent: percent)
 
-    price_rise >= one_percent_of_day1
+    price_rise >= percent_of_day1
   end
 end
