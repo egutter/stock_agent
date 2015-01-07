bc58bf6d242f42072a7f31eac8c1c7d605abb23d
Sirko Sittig <sirko.sittig@gmail.com>
2015-01-07 03:35:09 -0300

    remove cash instance variable and data_for method from stock_market class

--- spec/stock_market_spec.rb
+++ spec/stock_market_spec.rb
@@ -23,27 +23,6 @@ describe StockMarket do
 
     it 'sets the source data instance variable' do
       expect(stock_market.instance_variable_get("@filename")).to eq(filename)
-      expect(stock_market.instance_variable_get("@cash")).to eq(1000000.00)
-    end
-  end
-
-  describe '#data_for' do
-    context 'source data provides only one price for YPF and TS stock in july 2014' do
-      it 'returns array of all data for the given month and year' do
-        expect(stock_market.data_for(month: 7, year: 2014)).to eq(source_data)
-      end
-
-      it 'returns empty array for june 2014' do
-        expect(stock_market.data_for(month: 6, year: 2014)).to be_empty
-      end
-
-      it 'returns empty array for july 2015' do
-        expect(stock_market.data_for(month: 7, year: 2015)).to be_empty
-      end
-
-      it 'only returns YPF stock' do
-        expect(stock_market.data_for(stock: 'YPF', month: 7, year: 2014)).to eq([source_data.first])
-      end
     end
   end
 
--- stock_market.rb
+++ stock_market.rb
@@ -1,13 +1,6 @@
 class StockMarket
   def initialize(filename)
     @filename = filename
-    @cash      = 1000000.00
-  end
-
-  def data_for(stock: '*', month:, year:)
-    _data = source_data.reject{|row| row[1].month != month || row[1].year != year }
-    _data = source_data.reject{|row| row[0] != stock } if stock != '*'
-    _data
   end
 
   def source_data
