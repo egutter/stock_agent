8c1a921ff898a849b127192e4f70266520a0faa1
Sirko Sittig <sirko.sittig@gmail.com>
2015-01-08 12:36:55 -0300

    add stock method to calculate maximum amount of stocks for a cash limit

--- stock.rb
+++ stock.rb
@@ -16,13 +16,18 @@ class Stock
     (value1 - value2).round(2)
   end
 
+  def maximum_amount(cash_limit, price)
+    return 0 if !cash_limit.is_a?(Numeric) || !price.is_a?(Numeric) || price <= 0 || cash_limit <= 0
+    (cash_limit / price).to_i
+  end
+
   def price_change_for_day(day1)
     price_at_day1 = price_at(previous_day(day1))
     price_at_day2 = price_at(day1)
 
     return if price_at_day1.nil? || price_at_day2.nil?
 
-    diff(price_at_day2, price_at_day1) / calc_percents(number: price_at_day1, percent: 1)
+    (diff(price_at_day2, price_at_day1) / calc_percents(number: price_at_day1, percent: 1)).round(2)
   end
 
   def previous_day(date)
