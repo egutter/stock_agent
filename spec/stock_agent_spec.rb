ff58a47c449e0bccd463df641bda784b58db41b2
Sirko Sittig <sirko.sittig@gmail.com>
2015-01-08 14:50:41 -0300

    add buy and transfer money methods for agent

--- spec/stock_agent_spec.rb
+++ spec/stock_agent_spec.rb
@@ -15,6 +15,28 @@ describe StockAgent do
     end
   end
 
+  describe '#buy' do
+    before do
+      allow_any_instance_of(Stock).to receive(:price_at).and_return(10.00)
+    end
+
+    it 'purchase stocks for $1000.00' do
+      expect(agent.total_cash).to eq(1000000.00)
+      expect(agent.buy('YPF', '2014-04-02')).to eq(true)
+      expect(agent.total_cash).to eq(999000.00)
+    end
+
+    context 'agent has $1.00 start cash' do
+      let(:agent) { StockAgent.new(['YPF'], 1.00) }
+
+      it 'is not purchasing stocks' do
+        expect(agent.total_cash).to eq(1.00)
+        expect(agent.buy('YPF', '2014-04-02')).to eq(false)
+        expect(agent.total_cash).to eq(1.00)
+      end
+    end
+  end
+
   describe '#cash_for_purchase' do
     context 'purchase limit is default at $1000.00' do
       it 'returns $10.00 if total_cash is 10.00' do
@@ -27,6 +49,36 @@ describe StockAgent do
         allow(agent).to receive(:total_cash).and_return(10000.00)
 
         expect(agent.cash_for_purchase).to eq(1000.00)
+      end
+    end
+  end
+
+  describe '#transfer_money' do
+    context ':buy' do
+      let(:action) { :buy }
+
+      it 'returns nil if balance is smaller than value' do
+        expect(agent.transfer_money(100,1000, action)).to eq(nil)
+      end
+
+      it 'returns $900.00 if balance is $1000 and value $100' do
+        expect(agent.transfer_money(1000,100, action)).to eq(900)
+      end
+
+      it 'returns $0.00 if balance is $1000 and value $1000' do
+        expect(agent.transfer_money(1000,1000, action)).to eq(0)
+      end
+    end
+
+    context ':sell' do
+      let(:action) { :sell }
+
+      it 'returns $1100.00 if balance is $1000 and value $100' do
+        expect(agent.transfer_money(1000, 100, action)).to eq(1100)
+      end
+
+      it 'returns $0.00 if balance is $1000 and value $100' do
+        expect(agent.transfer_money(1000, 1000, action)).to eq(2000)
       end
     end
   end
--- stock_agent.rb
+++ stock_agent.rb
@@ -1,14 +1,42 @@
 class StockAgent
-  def initialize(stocks)
-    @total_cash   = 1000000.00
-    @stocks = stocks
+  def initialize(stocks, start_cash=1000000.00)
+    @total_cash   = start_cash
+    @stocks       = stocks
   end
 
   def total_cash
     @total_cash
   end
 
+  def buy(stock, date, old_balance=@total_cash)
+    price           = Stock.new(stock).price_at(date)
+    amount          = Stock.maximum_amount(cash_for_purchase, price)
+    price_of_stocks = amount * price
+
+    if @total_cash >= price_of_stocks && amount > 0
+      @total_cash = old_balance - price_of_stocks
+      save_transaction(date, stock, :buy, amount, price)
+
+      return true
+    end
+
+    false
+  end
+
   def cash_for_purchase(limit=1000.00)
     total_cash >= limit ? limit : total_cash
+  end
+
+  def transfer_money(account_balance, value, action)
+    case action
+    when :buy
+      return if account_balance < value
+      return account_balance - value
+    when :sell
+      return account_balance + value
+    end
+  end
+
+  def save_transaction(date, stock, action, amount, price)
   end
 end
